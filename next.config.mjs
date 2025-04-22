00815bd5b0b7e03d35f759603c1251bf0f88ea2d
Manuel Julio Casanova Reyes <manueljuliocasanovareyes@Manuels-MacBook-Pro.local>
2025-04-22 06:45:42 -0400

    modified nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,14 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "www.sds-construction.ca",
+        destination: "https://sds-construction.ca",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
