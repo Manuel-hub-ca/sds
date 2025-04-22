7cba2a4cb84d758e7a93e35ddbc95249e1f970dc
Manuel Julio Casanova Reyes <manueljuliocasanovareyes@Manuels-MacBook-Pro.local>
2025-04-22 08:54:05 -0400

    removed nextconfigcode

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,3 @@
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.sds-construction.ca" }],
-        destination: "https://sds-construction.ca/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
