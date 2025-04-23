8ca2b55e73d9e9b55f05bae57a38d4b648d64274
Manuel Julio Casanova Reyes <manueljuliocasanovareyes@Manuels-MacBook-Pro.local>
2025-04-23 18:40:11 -0400

    added-google-verification-file

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.sds-construction.ca",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 7000,
+  outDir: "public",
+};
